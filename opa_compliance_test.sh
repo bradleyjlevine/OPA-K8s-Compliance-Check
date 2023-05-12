92ea1037fcace04a3293c686cc294a261d309926
Bradley J. Levine <bradlevine25@gmail.com>
2023-05-12 14:35:26 -0400

    Add files via upload

---
+++ opa_compliance_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+test1=$(kubectl get pods -A -o json | jq --arg labelk gatekeeper.sh/system '.items |= map(select(.metadata.labels | .[$labelk]=="yes")) | [{name:.items[].metadata.name, ns:.items[].metadata.namespace, phase:.items[].status.phase, image:.items[].spec.containers[].image}]' | jq unique | jq '.[] | select(.phase=="Running")' | jq '. | length' | grep "4" -c)
+
+if [[ "$test1" > "0" ]]; then
+	echo "Passed! OPA Gatekeeper Found and Running."
+else
+	echo "Failed! OPA Gatekeepr not Found Running."
+fi
